3852abacf2c4cdbb5e9a2b3f3882ac0f533b5822
antromo <anthonyroma741@gmail.com>
2020-03-13 18:36:40 -0400

    first commit

---
+++ lib/SmashBrosCLI.rb
@@ -0,0 +1,10 @@
+#ENvironment file
+# in here loads all the files
+
+
+require "SmashBrosCLI/version"
+
+module SmashBrosCLI
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/SmashBrosCLI/version.rb
@@ -0,0 +1,3 @@
+module SmashBrosCLI
+  VERSION = "0.1.0.0"
+end
